5efdc4908c4ca0eb42aed1285b0642131953d034
michael <michael.qiao@vskysoft.com>
2019-11-05 23:45:20 +0800

    修改类名

--- AlgorithmPratice.Core/RemoveDuplicatesFromSortedArray.cs
+++ AlgorithmPratice.Core/RemoveDuplicatesFromSortedArray.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AlgorithmPractice.Core
 {
-    public class ArrayPractice
+    public class RemoveDuplicatesFromSortedArray
     {
         /********************************************
          * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。

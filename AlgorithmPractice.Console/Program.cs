ee1a6b2eeb98b701574047a9e24f029b48bf275e
michael <michael.qiao@vskysoft.com>
2019-11-05 23:32:04 +0800

    添加项目文件。

---
+++ AlgorithmPractice.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using AlgorithmPratice.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPractice.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 8, 8, 9 };
+            int arrayLength = ArrayPractice.RemoveDuplicatesV2(nums);
+            System.Console.WriteLine(arrayLength);
+
+            foreach (int item in nums)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ AlgorithmPratice.Core/ArrayPractice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPratice.Core
+{
+    public class ArrayPractice
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int j = 0;
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                if (nums[i] != nums[j])
+                {
+                    j++;
+                    nums[j] = nums[i];
+                }
+            }
+            return j;
+        }
+        public static int RemoveDuplicatesV2(int[] nums)
+        {
+            int j = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[j])
+                {
+                    j++;
+                    if (i - j > 0)
+                    {
+                        nums[j] = nums[i];
+                    }
+                }   
+            }
+            return j;
+        }
+    }
+}
